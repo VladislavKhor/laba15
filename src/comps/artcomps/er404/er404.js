92986f72d3c7b5cd509a93b4138f9c09075ff4a3
VladislavKhor <khorvladiv@gmail.com>
2025-05-10 13:08:58 +0300

    Добавил страницу для ошибки 404

---
+++ src/comps/artcomps/er404/er404.js
@@ -0,0 +1,7 @@
+export default function Er404(){
+    return(
+        <div className="er404">
+            <p>Просим прощения, такой статьи нет, проверьте вашу ссылку.</p>
+        </div>
+    );
+}
