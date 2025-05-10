a3008a6de66c4dd434961ae12bff0ff408b53c8f
VladislavKhor <khorvladiv@gmail.com>
2025-05-10 12:39:14 +0300

    first commit

---
+++ src/comps/artcomps/contacts/contacts.js
@@ -0,0 +1,7 @@
+export default function Contacts(){
+    return(
+        <div className="contacts">
+            <p>+79525770966</p>
+        </div>
+    );
+}
---
+++ src/comps/footer/footer.js
@@ -0,0 +1,13 @@
+import './footer.css'
+
+export default function Footer() {
+    
+    return (
+        <div className="footer">
+            <div>
+                <p>Сайт создан Хорошавиным Владиславом, студентом физфака ЮФУ, 8 мая 2025 года</p>
+            </div>          
+        </div>
+    );
+}
+
